56f33b8d6b17af1655d5bb0d4bc496e6138ad90b
jhchoi616 <jhchoi@idevel.kr>
2026-09-17 14:19:19 +0900

    [test] test router err

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,18 +1,22 @@
 import { PrimeReactProvider } from 'primereact/api'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {BrowserRouter, createBrowserRouter, RouterProvider} from 'react-router-dom'
+import {BrowserRouter, createBrowserRouter, RouterProvider, useRouteError} from 'react-router-dom'
 
 import App from './App.jsx'
 
 import 'primereact/resources/themes/lara-light-blue/theme.css'
 import 'primeicons/primeicons.css'
 import './styles.css'
-
+const CustomError = () => {
+  const error = useRouteError();
+  return <div>⚠️ 페이지를 로드할 수 없습니다.</div>;
+};
 const router = createBrowserRouter([
   {
     path: "*",
     element: <App />,
+    errorElement: < CustomError/>
   },
 ]);
 
